5d5c499fa93c61164ca2b3a9335f4f77e71a8f87
olya0292 <olya.gan92@gmail.com>
2017-03-21 20:04:13 +0200

    Create list user console command

--- application.php
+++ application.php
@@ -6,9 +6,14 @@ require __DIR__.'/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
 use AppBundle\Command\GetAlbumsCommand;
+use AppBundle\Command\ListUserCommand;
 
 $application = new Application();
 
 // register app:get-albums commands
 $application->add(new GetAlbumsCommand());
+
+// register app:list-users commands
+$application->add(new ListUserCommand());
+
 $application->run();
