13e8db638c874d8ae5cb58a70b79f7556aef25bd
olya0292 <olya.gan92@gmail.com>
2017-03-22 12:39:11 +0200

    Update user entity

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -23,7 +23,94 @@ class User
     private $name;
 
     /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $last_name;
+
+    /**
      * @ORM\Column(type="string", length=50)
      */
     private $vk_id;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set vkId
+     *
+     * @param string $vkId
+     *
+     * @return User
+     */
+    public function setVkId($vkId)
+    {
+        $this->vk_id = $vkId;
+
+        return $this;
+    }
+
+    /**
+     * Get vkId
+     *
+     * @return string
+     */
+    public function getVkId()
+    {
+        return $this->vk_id;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->last_name = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
 }
